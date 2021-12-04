950244b59d0004cdcab64e1f0417d1565cebd209
AmossDvir <amoss.dvir@mail.huji.ac.il>
2021-12-04 13:45:54 +0200

    Creating a client request

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-class App extends React.Component{
-
-    onSearchSubmit = (searchQuery) =>{
-        console.log(searchQuery);
+class App extends React.Component {
+  state = { images: [] }
+  onSearchSubmit = async (searchQuery) => {
+    console.log(searchQuery);
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+    params: { query: searchQuery },
+    headers:{
+      Authorization: "Client-ID k_gN-6Y8O-NynAXs6ooxn-MwnLnY7v3hrqCQ69f-6oo"
     }
-    render() {
+    });
+    console.log(this);
+    this.setState({ images:response.data.results });
+  }
+  render() {
     return (
       <div className="ui container" style={{ marginTop: "50px", width: "50%" }}>
         <SearchBar onSubmit={this.onSearchSubmit}></SearchBar>
+        Found { this.state.images.length } Images.
       </div>
     );
   }
